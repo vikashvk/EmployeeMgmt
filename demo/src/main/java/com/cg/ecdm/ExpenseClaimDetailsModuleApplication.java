4242d48316e9f98d5702a379dc2db49f872a4af1
Bajpai <anand.bajpai@capgemini.com>
2018-12-12 10:53:32 +0530

    Expense Claim

---
+++ demo/src/main/java/com/cg/ecdm/ExpenseClaimDetailsModuleApplication.java
@@ -0,0 +1,14 @@
+package com.cg.ecdm;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableEurekaClient
+@SpringBootApplication
+public class ExpenseClaimDetailsModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseClaimDetailsModuleApplication.class, args);
+	}
+}
